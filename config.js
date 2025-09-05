52013eb58451ce25aec74e13863235f1d1080ef9
Prathamesh2801 <truepratham2801@gmail.com>
2025-09-05 17:05:38 +0530

    Completed First Part and Transfered to Server

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 export const BASE_URL = "https://digitalvastr.com/API";
 
-// export const redirectToUserFlowURL = 'https://digitalvastr.com/#/startup'
-export const redirectToUserFlowURL = 'http://localhost:5174/#/startup'
+export const redirectToUserFlowURL = 'https://digitalvastr.com/#/startup'
+// export const redirectToUserFlowURL = 'http://localhost:5174/#/startup'
 // export const BASE_URL = "http://192.168.1.55/ClothSwap/API";
 // export const BASE_URL = "https://eventtechsolutions.in/API";
 // export const BASE_URL = "https://dbslfc2025.in/API";
